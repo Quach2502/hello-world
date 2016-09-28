c53ecb49fa59d634f8581a88e1907fa72aa0f82b
Quach <quachdung2502@gmail.com>
2016-09-28 13:37:37 +0800

    2809

---
+++ practiceCode/practiceCode/136A.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <vector>
+//void main()
+//{
+//	int number(0);
+//	int max = 1;
+//	std::vector<int> input;
+//	std::cin >> number;
+//	std::vector<int> output;
+//	bool flag = true;
+//	input.push_back(0);
+//	for (int i = 0; i < number; i++)
+//	{
+//		int a;
+//		std::cin >> a;
+//		input.push_back(a);
+//	}
+//	output = input;
+//	for (int i = 1; i <= number; i++)
+//	{
+//		output[input[i]] = i;
+//	}
+//	for (int i = 1; i <= number; i++)
+//		std::cout << output[i]<<" ";
+//}
---
+++ practiceCode/practiceCode/719A.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//#include <vector>
+//
+//void main()
+//{
+//	int number(0);
+//	std::vector<int> input;
+//	std::cin >> number;
+//	int output = 1;
+//	bool flag = true;
+//	for (int i = 0; i < number; i++)
+//	{
+//		int a;
+//		std::cin >> a;
+//		input.push_back(a);
+//	}
+//	if ((input[number - 1] == 0) || (input[number - 1]==15))
+//	{
+//		if (input[number - 1] == 0)
+//			std::cout << "UP";
+//		else
+//			std::cout << "DOWN";
+//	}
+//	else
+//	{
+//		if (number==1)
+//			std::cout << "-1";
+//		else
+//		{
+//			if(input[number-2]> input[number-1])
+//				std::cout << "DOWN";
+//			else
+//				std::cout << "UP";
+//		}
+//	}
+//}
