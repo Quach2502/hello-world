7c99edf39112011a780401d775a7feb8987f1bd1
Quach <quachdung2502@gmail.com>
2016-11-11 17:39:35 +0800

    fix

---
+++ practiceCode/practiceCode/A682.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <set>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//#include <unordered_map>
+//#include <unordered_set>
+//typedef long long ll;
+//void main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::unordered_map<std::string, std::string> dict;
+//	int n,m;std::cin>>n>>m;
+//	int max = 1;
+//	std::vector<ll> input;
+//	ll output(0);
+//	bool flag = true;
+//	int r = m % 5;
+//	int s = m / 5;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int remain = i % 5;
+//		output += s;
+//		if (remain + r >= 5)
+//			output++;
+//	}
+//	std::cout << output;
+//	
+//}
---
+++ practiceCode/practiceCode/C682.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+typedef long long ll;
+void main()
+{
+	std::ios::sync_with_stdio(false);
+	std::unordered_map<std::string, std::string> dict;
+	int n(0); std::cin >> n;
+	int max = 1;
+	int current(0);
+	std::vector<ll> input;
+	int output;
+	bool flag = true;
+	for (int i = 0; i < n; i++)
+	{
+		int a; std::cin >> a;
+		input.push_back(a);
+	}
+	std::sort(input.begin(), input.end());
+	for (int i = 0; i < n; i++)
+	{
+		if (input[i] > current)
+			current++;
+	}
+	std::cout << current + 1;
+}
