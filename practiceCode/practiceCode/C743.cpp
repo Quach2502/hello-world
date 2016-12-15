dca7a7fc7b555dec62b64c8769dd7358e317340d
Quach <quachdung2502@gmail.com>
2016-12-15 12:04:20 +0800

    fix

---
+++ practiceCode/practiceCode/C743.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <set>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//#include <unordered_map>
+//#include <unordered_set>
+//typedef long long ll;
+//void main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::unordered_map<std::string, std::string> dict;
+//	int n(0);std::cin>>n;
+//	if (n == 1) std::cout << -1;
+//	else
+//	{
+//		std::cout << n << " " << n + 1 << " " << n*(n + 1);
+//	}
+//}
