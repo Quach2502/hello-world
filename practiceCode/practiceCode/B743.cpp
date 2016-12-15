849df355bd7d59777fb93f21e5d26e96370da9b0
DungQuach <quachdung2502@gmail.com>
2016-12-16 00:11:45 +0800

    fix

--- practiceCode/practiceCode/B743.cpp
+++ practiceCode/practiceCode/B743.cpp
@@ -1,33 +1,33 @@
-//#include <iostream>
-//#include <set>
-//#include <vector>
-//#include <algorithm>
-//#include <string>
-//#include <unordered_map>
-//#include <unordered_set>
-//#include <cmath>
-//#define E 1e-10
-//typedef long long ll;
-//int logt(ll x)
-//{
-//	int r(0);
-//	int temp(1);
-//	while(temp <= x)
-//	{
-//		temp *= 2; r++;
-//	}
-//	if(temp > x) r--; 
-//	return r;
-//}
-//void main()
-//{
-//	std::ios::sync_with_stdio(false);
-//	std::unordered_map<std::string, std::string> dict;
-//	ll n,k;std::cin>>n>>k;
-//	while(!(abs(k - int(pow(2.0,int(log2(k))))) < E))
-//	{
-//		k -= int(pow(2.0,int(log2(k))));
-//		std::cout<<k;
-//	}
-//	std::cout<<int(log2(k))+1;
-//}
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#define E 1e-10
+typedef long long ll;
+int logt(ll x)
+{
+	int r(0);
+	int temp(1);
+	while(temp <= x)
+	{
+		temp *= 2; r++;
+	}
+	if(temp > x) r--; 
+	return r;
+}
+void main()
+{
+	std::ios::sync_with_stdio(false);
+	std::unordered_map<std::string, std::string> dict;
+	ll n,k;std::cin>>n>>k;
+	while(!(abs(k - int(pow(2.0,int(log2(k))))) < E))
+	{
+		k -= int(pow(2.0,int(log2(k))));
+		std::cout<<k;
+	}
+	std::cout<<int(log2(k))+1;
+}
